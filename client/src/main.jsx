6f8e4245f4e38f16077fe33490c8a899affbd0ad
vickykumar-2003 <vickyk15009@gmail.com>
2026-04-02 08:09:11 +0530

    done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import axios from 'axios'
+
+// Point to deployed backend in production, otherwise use relative path (Vite proxy)
+axios.defaults.baseURL = import.meta.env.PROD ? 'https://assingment-acap.onrender.com' : '';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
